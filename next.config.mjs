5b394e6f7002ca785404d027456409cbf984e4b3
UNAINR <munain605@gmail.com>
2024-10-16 01:26:11 +0500

    fyf

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
     //   // Important: return the modified config
     //   return config
     // },
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
   }
 
 
