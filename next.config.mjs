ec0a26831bc265ca805f6295b38e0070e6ff25b8
UNAINR <munain605@gmail.com>
2024-10-16 01:58:06 +0500

    kkk

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,20 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    // webpack: (
-    //   config,
-    //   { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-    // ) => {
-    //   if (config.cache && !dev) {
-    //     config.cache = Object.freeze({
-    //       type: 'memory',
-    //     })
-    //   }
-    //   // Important: return the modified config
-    //   return config
-    // },
+    experimental: {
+      appDir: true,
+      esmExternals: "loose",
+    },
+    env: {
+      DB_URI: process.env.MONGO_URI,
+    },
+    webpack: (config) => {
+      config.experiments = {
+        topLevelAwait: true,
+        layers: true,
+      };
+      return config;
+    },
   }
 
 
