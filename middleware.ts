c559d53b97e55ff09c1ecdeb192e644976f33175
UNAINR <munain605@gmail.com>
2024-10-16 12:48:38 +0500

    yu

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,10 @@ import { NextResponse } from 'next/server';
 export function middleware(req:any) {
   const token = req.cookies.get('token');
 
+  if (!token && req.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
   return NextResponse.next();
 }
 
